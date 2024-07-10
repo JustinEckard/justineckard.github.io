e91dd09f8178ab754f50fd3bfb36556956798d37
JustinEckard <justinweckard@gmail.com>
2024-07-10 22:41:13 +0200

    Work section and education section added

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 jQuery(function() {
-    // Hide all "More Details" sections initially
     $('.more-details').hide();
 
-    $('.more-details-button').on('click touchstart', function() {
-      const moreDetailsSection = $(this).closest('.work-item').find('.more-details');
+    $('.more-details-button').on('click ', function() {
+      const moreDetailsSection = $(this).closest('.info-item').find('.more-details');
       moreDetailsSection.slideToggle();
 
       $(this).find('.fa-solid.fa-angle-down').toggleClass('fa-rotate-180');
